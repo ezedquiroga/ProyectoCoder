d32527a8db8b2bb03a5f54fc2da74c95a3c94216
Ezequiel Quiroga <ezedaqui@gmail.com>
2023-03-18 22:29:55 -0300

    Creando plantillas heredadas

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from AppCoder.views import cursos, estudiantes, profesores
+from AppCoder.views import cursos, estudiantes, profesores, crear_curso
 
 urlpatterns = [
-    path('cursos', cursos),
-    path('estudiantes', estudiantes),
-    path('profesores', profesores),
+    path('cursos', cursos, name="AppCoderCursos"),
+    path('curso/<nombre>/<camada>', crear_curso, name="AppCoderCurs"),
+    path('estudiantes', estudiantes, name="AppCoderEstudiantes"),
+    path('profesores', profesores, name="AppCoderCursosProfesores"),
 ]
